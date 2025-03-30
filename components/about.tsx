eed6f46242e36be5313e636f861bfaa724b59f45
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-03-30 12:23:40 +0100

    Fix: about.tsx mobile reponsiveness.

--- components/about.tsx
+++ components/about.tsx
@@ -6,18 +6,18 @@ import { footerImg } from "@/assets";
 
 const About = () => {
   return (
-    <div className="bg-slate-100 text-primary flex flex-col justify-center items-center pt-24 md:px-[20%]">
-      <h2 className="text-6xl text-center font-extrabold px-20 mb-6 w-fit">
+    <div className="bg-slate-100 text-primary flex flex-col justify-center items-center pt-12 md:pt-24 md:px-[20%]">
+      <h2 className="text-2xl md:text-6xl text-center font-extrabold md:px-20 mb-3 md:mb-6 w-fit">
         More than a link shortener
       </h2>
-      <p className="text-2xl px-32 mb-6 text-center">
+      <p className="text-lg md:text-2xl px-3 md:px-32 md:mb-6 text-center">
         Knowing how your clicks and scans are performing should be as easy as
         making them. Track, analyze, and optimize all your connections in one
         place.
       </p>
       <Button
         size={"lg"}
-        className="bg-primary font-semibold capitalize my-12 cursor-pointer"
+        className="bg-primary font-semibold capitalize my-10 md:my-12 cursor-pointer md:mb-0"
       >
         Get started for free <ArrowRightIcon />
       </Button>
@@ -26,6 +26,7 @@ const About = () => {
         height={footerImg.height}
         width={footerImg.width}
         alt="App image"
+        className="hidden md:block"
       />
     </div>
   );
